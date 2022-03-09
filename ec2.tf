1963a1331b3d83c7e1409280e657774b693814b5
tejasghadi <tejas.ghadi1990@gmail.com>
2022-03-09 17:13:05 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "web" {
+  ami           = "ami-04505e74c0741db8d"
+  instance_type = "t2.micro"
+  key_name      = "terraform_acloudguru"
+  tags = {
+    Name = "web_ec2"
+  }
+}
+
+provider "aws" {
+  region     = "us-east-1"
+}
