33ffbdf8db1266298405dfddf806a1370f3d7e8b
tejasghadi <tejas.ghadi1990@gmail.com>
2022-03-10 15:39:16 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "web" {
   ami           = "ami-04505e74c0741db8d"
   instance_type = "t2.micro"
   key_name      = "terraform_acloudguru"
+  user_data     = "${file("commands.sh")}"
   tags = {
     Name = "web_ec2"
   }
