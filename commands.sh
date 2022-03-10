2abac4af5919b576e2ea7d0ca789e71f58979c0e
tejasghadi <tejas.ghadi1990@gmail.com>
2022-03-10 15:38:02 +0530

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+apt update && apt upgrade -y
+
+apt-get install -y \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+    
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get update && apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sleep 5
+docker run hello-world > /tmp/docker_pod.log
+
+
