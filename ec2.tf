263768d14d78f0a698dd0bb9f09a66a381978e84
tejasghadi <tejas.ghadi1990@gmail.com>
2022-03-10 13:34:04 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = ${ACCESS_KEY}
-  secret_key = ${SECRET_KEY}
+  access_key = "${env.ACCESS_KEY}"
+  secret_key = "${env.SECRET_KEY}"
 }
 
 resource "aws_instance" "web" {
